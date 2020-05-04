13a7a404f3457d98daec3e118e998068d6dce382
hax0kartik <pokemonemerald0@gmail.com>
2020-05-05 02:18:18 +0530

    change code a bit

--- script.py
+++ script.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 
 url = "https://hackerone.com/nintendo/hacktivity"
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(30)
 driver.get(url)
+wait = WebDriverWait(webdriver, 10)
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'daisy-helper-text')))
+
+print(driver.page_source)
 
 f = open("/build_dir/hacktivity", "w")
 f.write(driver.page_source)
