ddc46993776b92a669e797b782a1f388b2993746
hax0kartik <pokemonemerald0@gmail.com>
2020-05-05 01:59:59 +0530

    Didn't work, try using chromium xvfb

--- ghost_script.py
+++
@@ -1,9 +0,0 @@
-from ghost import Ghost
-ghost = Ghost()
-
-with ghost.start() as session:
-    page, extra_resources = session.open("https://hackerone.com/nintendo/hacktivity")
-    f = open("/build_dir/hacktivity", "wb")
-    f.write(page.content)
-    f.close()
-    assert page.http_status == 200
---
+++ script.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+url = "https://hackerone.com/nintendo/hacktivity"
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(30)
+driver.get(url)
+
+f = open("/build_dir/hacktivity", "wb+")
+f.write(driver.page_source)
+f.close()
