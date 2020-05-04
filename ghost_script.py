e1c335bbd6eacd00fddd720e612803917ee1ac61
hax0kartik <pokemonemerald0@gmail.com>
2020-05-05 00:30:18 +0530

    Open file in binary mode

--- ghost_script.py
+++ ghost_script.py
@@ -3,7 +3,7 @@ ghost = Ghost()
 
 with ghost.start() as session:
     page, extra_resources = session.open("https://hackerone.com/nintendo/hacktivity")
-    f = open("/build_dir/hacktivity", "w")
+    f = open("/build_dir/hacktivity", "wb")
     f.write(page.content)
     f.close()
     assert page.http_status == 200 and 'Reports resolved' in page.content
