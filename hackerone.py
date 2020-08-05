76b9f7d7a0d7a2103d4e5489e2aba517a455147d
hax0kartik <pokemonemerald0@gmail.com>
2020-08-05 11:39:24 +0530

    small fix

--- hackerone.py
+++ hackerone.py
@@ -49,7 +49,7 @@ elif hacktivity_l[0] != hacktivity_l[1]:
 
 if to_gen == 0:
     print("No new reports.")
-    open("./commit-message.txt", "w").write("N").close()
+    open("./commit-message.txt", "w").write("N")
     exit (0)
 
 print(str(to_gen) + " new reports.")
